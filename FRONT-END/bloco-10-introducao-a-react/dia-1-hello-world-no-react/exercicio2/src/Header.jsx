57776e190308d13fc2f536780b5a24801635b58e
alanymara <alanymara@gmail.com>
2022-01-24 14:33:10 -0300

    Exercicios sobre react

---
+++ FRONT-END/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio2/src/Header.jsx
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+class Header extends Component {
+    render(){
+        return (
+            <div className='title'>
+                 <h1>Conteúdos de Front-End</h1>
+            </div>
+         )}
+}
+export default Header;
